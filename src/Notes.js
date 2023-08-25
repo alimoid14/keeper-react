d10532f3d839474b9183008a4962a348656bdb55
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2023-08-25 20:19:48 +0530

    Render the stored notes

---
+++ src/Notes.js
@@ -0,0 +1,16 @@
+const Notes = [
+    {id:1,
+    heading: "Note1",
+    content: "Content1"},
+    {id:2,
+        heading: "Note2",
+        content: "Content2"},
+        {id:3,
+            heading: "Note3",
+            content: "Content3"},
+            {id:4,
+                heading: "Note4",
+                content: "Content4"}
+];
+
+export default Notes;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../Notes";
+
+function createNote(note){
+  return(<Note 
+    key={note.id}
+    heading={note.heading}
+    content={note.content}
+  />);
+}
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {Notes.map(createNote)}
       <Footer />
     </div>
   );
