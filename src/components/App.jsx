3697891bda016af497c6bd02509af36a98b0f1f3
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2023-08-25 20:30:41 +0530

    Simplify createNote using arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,17 @@ import Footer from "./Footer";
 import Note from "./Note";
 import Notes from "../Notes";
 
-function createNote(note){
-  return(<Note 
-    key={note.id}
-    heading={note.heading}
-    content={note.content}
-  />);
-}
-
 export default function App() {
   return (
     <div>
       <Header />
-      {Notes.map(createNote)}
+      {Notes.map((note)=>{
+        return(<Note
+          key={note.id}
+          heading={note.heading}
+          content={note.content}
+        />)
+      })}
       <Footer />
     </div>
   );
