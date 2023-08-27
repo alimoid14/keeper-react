4254eaa7a4daed12cf5f8533e4ea25831693f847
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2023-08-27 21:51:19 +0530

    Correct file name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import InputNote from "./inputNote";
+import InputNote from "./InputNote";
 
 export default function App() {
   
